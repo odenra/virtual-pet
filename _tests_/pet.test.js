ed5c9105553f11a6a7497ee348008b791109b51c
odenra <ilkerkaraoden@gmail.com>
2023-11-23 18:17:53 +0300

    name-test

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -1,7 +1,13 @@
-const Pet = require('../src/pet');
+const Pet = require("../src/pet");
 
 describe("constructor", () => {
   it("returns an object", () => {
     expect(new Pet("Fido")).toBeInstanceOf(Object);
+  });
+
+  it("sets the name property", () => {
+    const pet = new Pet("Fido");
+
+    expect(pet.name).toEqual("Fido");
   });
 });

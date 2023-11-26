d5e696788e17ab41295d5c1c972a3d32fe55b164
odenra <ilkerkaraoden@gmail.com>
2023-11-26 22:16:58 +0300

    commit

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -39,3 +39,23 @@ describe("growUp", () => {
     expect(pet.age).toEqual(1);
   });
 });
+
+describe('walk', () => {
+  it('increases fitness by 4', () => {
+    const pet = new Pet('Fido');
+
+    pet.fitness = 4;
+    pet.walk();
+
+    expect(pet.fitness).toEqual(8);
+  });
+
+  it('increases fitness by to a maximum of 10', () => {
+    const pet = new Pet('fido');
+
+    pet.fitness = 8;
+    pet.walk();
+
+    expect(pet.fitness).toEqual(10);
+  });
+});
